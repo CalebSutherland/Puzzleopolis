9b4e3f89c010fbf675b7a438d2b2e5fc8840c46f
Kyle Pinkham <kpinkham13@gmail.com>
2024-05-11 17:45:44 -0700

    Reverting to first commit

---
+++ Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    public GameObject player;
+    public Transform holdPos;
+
+
+}
