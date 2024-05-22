20c80a4656c4800dc28963ba83d39659ec06c858
CalebSutherland <sutherlandcaleb12@gmail.com>
2024-05-22 15:00:23 -0700

    PickUp Clipping Fixed
    New platfroming bug where platforms get out of sync, probably due to fixed update for the elevator script, should be a way to move all at the same time/rate while keeping fixed update

--- Assets/Scripts/ObjectPickup.cs
+++ Assets/Scripts/ObjectPickup.cs
@@ -6,11 +6,13 @@ public class ObjectPickup : MonoBehaviour
 {
     private Rigidbody objectRigidbody;
     private Transform objectGrabPointTransform;
+    private Collider objectCollider;
 
     private float lerpSpeed = 10f;
     private void Awake()
     {
         objectRigidbody = GetComponent<Rigidbody>();
+        objectCollider = GetComponent<Collider>();
     }
 
     public void Grab(Transform objectGrabPointTransform)
@@ -18,6 +20,7 @@ public class ObjectPickup : MonoBehaviour
         this.objectGrabPointTransform = objectGrabPointTransform;
         objectRigidbody.useGravity = false;
         objectRigidbody.isKinematic = true;
+        StartCoroutine(DelayedDisableCollider());
     }
 
     public void Drop()
@@ -25,6 +28,7 @@ public class ObjectPickup : MonoBehaviour
         this.objectGrabPointTransform = null;
         objectRigidbody.useGravity = true;
         objectRigidbody.isKinematic = false;
+        objectCollider.enabled = true;
     }
 
     private void FixedUpdate()
@@ -34,5 +38,11 @@ public class ObjectPickup : MonoBehaviour
             Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.deltaTime * lerpSpeed);
             objectRigidbody.MovePosition(newPosition);
         }
+    }
+
+    private IEnumerator DelayedDisableCollider()
+    {
+        yield return new WaitForSeconds(0.1f); // Delay of 0.1 second
+        objectCollider.enabled = false;
     }
 }
