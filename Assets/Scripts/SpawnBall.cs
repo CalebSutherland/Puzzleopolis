7a62bd9d971648c81637e69b2793c9550fe16e4e
CalebSutherland <sutherlandcaleb12@gmail.com>
2024-05-31 22:45:43 -0700

    Updated Ball Dispenser

--- Assets/Scripts/SpawnBall.cs
+++ Assets/Scripts/SpawnBall.cs
@@ -9,9 +9,15 @@ public class SpawnBall : MonoBehaviour
     public Collider playerCollider;
     public Transform spawnPoint;
 
+    public bool dispenseAlongX = true; // True for dispensing along x axis, false for z axis
+
     public float forceX = -3f;
     public float minZForce = -1f;
     public float maxZForce = 1f;
+    public float forceZ = -3f;
+    public float minXForce = -1f;
+    public float maxXForce = 1f;
+
     public void Spawn()
     {
         GameObject newBall = Instantiate(ball, spawnPoint.position, spawnPoint.rotation);
@@ -27,12 +33,23 @@ public class SpawnBall : MonoBehaviour
 
         Rigidbody rb = newBall.GetComponent<Rigidbody>();
 
-        if (rb != null) {
-            float randomZForce = Random.Range(minZForce, maxZForce);
-            Vector3 force = new Vector3(forceX, 0, randomZForce);
+        if (rb != null)
+        {
+            Vector3 force;
+            if (dispenseAlongX)
+            {
+                // Dispense along the x axis with a random z value
+                float randomZForce = Random.Range(minZForce, maxZForce);
+                force = new Vector3(forceX, 0, randomZForce);
+            }
+            else
+            {
+                // Dispense along the z axis with a random x value
+                float randomXForce = Random.Range(minXForce, maxXForce);
+                force = new Vector3(randomXForce, 0, forceZ);
+            }
 
             rb.AddForce(force, ForceMode.Impulse);
         }
     }
-
 }
