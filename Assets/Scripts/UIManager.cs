6b228b9194e375076133257a77f6db069cb66abf
CalebSutherland <sutherlandcaleb12@gmail.com>
2024-06-07 01:52:27 -0700

    Added basic pause menu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    [SerializeField] private GameObject hudCanvas = null;
+    [SerializeField] private GameObject pauseCanvas = null;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P) && !isPaused)
+        {
+            SetActivePause(true);
+        } else if (Input.GetKeyDown(KeyCode.P) && isPaused)
+        {
+            SetActivePause(false);
+        }
+    }
+
+    private void Start()
+    {
+        SetActiveHud(true);
+    }
+
+    public void SetActiveHud(bool state)
+    {
+        hudCanvas.SetActive(state);
+        pauseCanvas.SetActive(!state);
+    }
+
+    public void SetActivePause(bool state)
+    {
+        pauseCanvas.SetActive(state);
+        hudCanvas.SetActive(!state);
+
+        Time.timeScale = state ? 0 : 1;
+        isPaused = state;
+
+        Cursor.lockState = state ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = state;
+    }
+
+    public void Restart()
+    {
+        SetActivePause(false);
+        SceneManager.LoadScene(1);
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
